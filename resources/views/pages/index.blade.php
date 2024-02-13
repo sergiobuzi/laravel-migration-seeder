cd69b3778a09c05efc3711091a1ae94c342d4b4e
SergioBuzi <sergio.buzi97@gmail.com>
2024-02-13 18:23:19 +0100

    style: cards

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,10 +3,16 @@
     <title>Home</title>
 @endsection
 @section('content')
-    <h1>Treni</h1>
-    <ul>
+    <h1>Treni partenti alle ore 10</h1>
+    <ul class="row d-flex justify-content-center">
         @foreach ($trains as $train)
-            <li>{{$train -> azienda}}</li>
+            <li class="col-2 card p-2 m-1">
+                <h1>{{$train -> azienda}}</h1>
+                <h3>{{$train -> stazione_di_partenza}}</h3>
+                <h3>{{$train -> stazione_di_arrivo}}</h3>
+                <h4>{{$train -> orario_di_partenza}}</h4>
+                <h4>{{$train -> orario_di_arrivo}}</h4>
+            </li>
         @endforeach
     </ul>
 @endsection
